6ebc6c60ed0f65eff6724d83614f014ac2075b69
mertz99 <mairiertz@gmail.com>
2024-09-15 20:08:52 +0200

    api and axios update

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
+function updateWeatherInfo(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTMl = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "ec2cd7562bc7e10e8tb7o4222f23fa90";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
