df285c1ecd9ec89e2e0f2d2fcae2d72dcae820a6
mertz99 <mairiertz@gmail.com>
2024-09-22 23:45:02 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -58,7 +58,7 @@ function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
 
-  return days[date.getDate()];
+  return days[date.getDay()];
 }
 
 function getForecast(city) {
@@ -78,9 +78,7 @@ function displayForecast(response) {
     <div class="weather-forecast-day">
             <div class="weather-forecast-date">${formatDay(day.time)}</div>
             
-            <img src="${
-              day.condition.icon_url
-            }" class="weather-forecast-icon""/>
+            <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
             <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
                 <strong>${Math.round(day.temperature.maximum)}º</strong>
